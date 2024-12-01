e6722eadfd20b36d890b13a304a6da38946868eb
ixtk <ikhcamot@gmail.com>
2024-12-01 15:29:46 +0400

    Another change

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import "./App.css"
 import { AddToCart, RemoveItem } from "./Icons"
 import dessertsProducts from "./data.json"
 
-// console.log(dessertsProducts)
-
 // const BASE_URL = "https://res.cloudinary.com/dc2c49xov/desserts"
 
 function App() {
   return (
     <>
       <h1>Hello World ❄</h1>
-      {/* <img src={`${BASE_URL}/brownie-mobile.jpg`} alt="" /> */}
       <RemoveItem />
     </>
   )
