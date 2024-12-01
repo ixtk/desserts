2dadae3ddc5588c0725517a5274e65f8fbe277e5
ixtk <ikhcamot@gmail.com>
2024-12-01 15:28:03 +0400

    Test change

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,13 @@ import dessertsProducts from "./data.json"
 
 // console.log(dessertsProducts)
 
-const BASE_URL = "https://res.cloudinary.com/dc2c49xov/desserts"
+// const BASE_URL = "https://res.cloudinary.com/dc2c49xov/desserts"
 
 function App() {
   return (
     <>
-      <h1>
-        Hello World <AddToCart />
-      </h1>
+      <h1>Hello World ❄</h1>
       {/* <img src={`${BASE_URL}/brownie-mobile.jpg`} alt="" /> */}
-      {dessertsProducts.map((dessert) => {
-        const imageUrl = `${BASE_URL}/${dessert.images.thumbnail}`
-
-        return (
-          <p key={dessert.name}>
-            {/* <img src={BASE_URL + dessert.images.thumbnail} alt="" /> */}
-            <img src={imageUrl} alt="" />
-          </p>
-        )
-      })}
       <RemoveItem />
     </>
   )
